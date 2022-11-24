202a0c90507536a17800a1e2bc3803e500ccd8f6
Nyx <nyxfor13days@gmail.com>
2022-11-24 16:44:12 +0530

    Added Base Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,36 @@
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  darkMode: true,
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      screens: { DEFAULT: "48rem" },
+      borderRadius: { DEFAULT: 0 },
+      blur: { DEFAULT: "1rem" },
+      gap: { DEFAULT: "1rem" },
+      colors: ({ colors }) => ({
+        background: colors.zinc[900],
+        onBackground: colors.zinc[300],
+        surface: colors.zinc[800],
+        onSurface: colors.zinc[100],
+        surfaceVariant: colors.zinc[300],
+        onSurfaceVariant: colors.zinc[900],
+        primary: colors.emerald[400],
+        onPrimary: colors.zinc[900],
+      }),
+      spacing: {
+        xs: "0.25rem",
+        sm: "0.5rem",
+        md: "1rem",
+        lg: "1.25rem",
+        xl: "1.5rem",
+      },
+    },
   },
   plugins: [],
 };
