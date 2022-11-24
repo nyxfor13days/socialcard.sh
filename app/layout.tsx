24016078bbc555ba0bc839e2bf42bebb13c9973f
Nyx <nyxfor13days@gmail.com>
2022-11-24 12:46:28 +0530

    Setup the barebones of Next App

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+const RootLayout = ({ children }) => {
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<title>socialcard.sh</title>
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+};
+
+export default RootLayout;
