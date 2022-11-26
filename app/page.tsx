6a7ef1dd02683f1f160231277aea7913267b6fc8
Nyx <nyxfor13days@gmail.com>
2022-11-26 22:07:08 +0530

    Added Coming Soom

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import ComingSoon from "components/ComingSoon";
 
 const Home = () => {
-  return <main>Hello World</main>;
+  return (
+    <main className="container max-w-screen-md">
+      <section className="flex items-center h-screen">
+        <ComingSoon />
+      </section>
+    </main>
+  );
 };
 
 export default Home;
